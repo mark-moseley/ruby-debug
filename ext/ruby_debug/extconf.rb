db821e7efa90438e372458a5a23092bc919ba805
Mark Moseley <mark@fast-software.com>
2009-08-16 20:47:00 -0700

    ruby_core_source updates

--- ext/ruby_debug/extconf.rb
+++ ext/ruby_debug/extconf.rb
@@ -1,33 +1,31 @@
 require "mkmf"
 require "ruby_core_source"
 
-if RUBY_VERSION >= "1.9"
-  if RUBY_RELEASE_DATE < "2005-03-17"
-    STDERR.print("Ruby version is too old\n")
-    exit(1)
-  end
-elsif RUBY_VERSION >= "1.8"
-  if RUBY_RELEASE_DATE < "2005-03-22"
-    STDERR.print("Ruby version is too old\n")
-    exit(1)
-  end
-else
+if RUBY_VERSION < "1.9"
   STDERR.print("Ruby version is too old\n")
   exit(1)
 end
 
 dir_config("ruby")
-with_cppflags("-I" + Ruby_core_source::get_ruby_core_source) {
-  if have_header("vm_core.h") and have_header("iseq.h") and have_header("insns.inc") and 
-    have_header("insns_info.inc")
+if have_header("vm_core.h") and have_header("iseq.h") and have_header("insns.inc") and 
+  have_header("insns_info.inc")
 
-    create_makefile("ruby_debug")
-  else
-    STDERR.print("Makefile creation failed\n")
-    STDERR.print("*************************************************************\n\n")
-    STDERR.print("  NOTE: For Ruby 1.9 installation instructions, please see:\n\n")
-    STDERR.print("     http://wiki.github.com/mark-moseley/ruby-debug\n\n")
-    STDERR.print("*************************************************************\n\n")
-    exit(1)
-  end
-}
+  create_makefile("ruby_debug")
+else
+  with_cppflags("-I" + Ruby_core_source::get_ruby_core_source) {
+
+    if have_header("vm_core.h") and have_header("iseq.h") and have_header("insns.inc") and 
+      have_header("insns_info.inc")
+
+      create_makefile("ruby_debug")
+    else
+
+      STDERR.print("Makefile creation failed\n")
+      STDERR.print("*************************************************************\n\n")
+      STDERR.print("  NOTE: For Ruby 1.9 installation instructions, please see:\n\n")
+      STDERR.print("     http://wiki.github.com/mark-moseley/ruby-debug\n\n")
+      STDERR.print("*************************************************************\n\n")
+      exit(1)
+    end
+  }
+end
